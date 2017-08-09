1f0cb58bbbd7ab35b2f8ab7c0af0fd6ca63cb9e8
docljn <ljnoble@gmail.com>
2017-08-09 22:23:44 +0100

    First commit with aims

---
+++ secret.rb
@@ -0,0 +1,10 @@
+#AIM
+#Write a secret santa program to ensure that each person gives a gift to one other
+#and each person receives a gift from one other
+
+#extension: the output will be blind, so that if you type in your name, you are given 
+#the option to type in your wishlist, and then given
+#the name of your recipient and their wishlist
+
+#extension: if the people are in groups (families/teams etc, ensure that most gifts cross groups)
+
