6e532285e8afcc498594402fb026fe0636f9d016
docljn <ljnoble@gmail.com>
2017-08-14 22:36:20 +0100

    trying to refactor using methods

--- secret.rb
+++ secret.rb
@@ -10,10 +10,13 @@
 
 #WORKING IDEAS HERE
 #could create more than one array, each with a family name, to separate out families
-#maybe test first with A = ["a1", "a2", "a3"]; B = ["b1", "b2", "b3"]
+#maybe test first with a = ["a1", "a2", "a3"]; b = ["b1", "b2", "b3"]
 #then add C = ["c1", "c2"] to get mismatch in group sizes
 
-#input (part3) would ask people to select which group they were part of, or to add a new group if not there?
+#OR start thinking about objects instead:
+#person(name, clan, wishlist)??
+#and what about writing methods so that they can be reused.
+#that might mean a whole rewrite though....
 
 #difficult bit is going to be selecting from all other families so no pattern and no internal gifts
 
@@ -37,42 +40,48 @@ people = ["John", "Lorna", "Frances", "Bine"]
 #create a giver<->recipient reference list (a hash) ready to use
 list = Hash.new
 
-
 #PROCESS FOR GENERATING PAIRS
-#1: use the list of people to create a scratch list identical to the people: givers and recipients
+#use the list of people to create a scratch list identical to the people: givers and recipients
 #can't just make them equal as then you've modified both!!!!!
-
-givers = people[0..-1]
-recipients = people[0..-1]
-
 #Also create two empty arrays to store the names you've used so that you don't use them again.
+#because I need access to santa and opener outside the methods, I need to initialise them here
+#but I also need to access them within the method, so what do I do????
 given = []
 received = []
 done = people.length
+santa = 'santa'
+opener = 'opener'
+
+def choose_santa(people,given)
+	givers = people[0..-1]
+	p givers
+	santas = givers - given
+	p santas
+	santa = santas.sample
+	p santa
+end
+
+def choose_opener(people,received)
+	recipients = people[0..-1]
+	p recipients
+	openers = recipients - received
+	p openers
+	openers.delete santa
+	p openers
+	opener = openers.sample
+	p opener
+end
 
 loop do 
 
-	#remove anyone who has already given a present from your list of potential santas
-	santas = givers - given
-	#Use array.sample to select a random giver
-	santa = santas.sample
-	#add the giver to the list of people who have given presents
+	choose_santa(people,given)
 	given << santa
 
-
-	#remove anyone who has already received a present from your list of potential openers
-	openers = recipients - received
-	#delete santa from the list of recipients (can't give to yourself) 
-	openers.delete santa
-	#select a random recipient 
-	opener = openers.sample
-	#add the recipient to the list of people who have got presents
+	choose_opener(people, received)
 	received << opener
-
 
 	#add the pair to the hash
 	list[opener] = santa
-
 
 break if done == list.length
 
@@ -88,6 +97,7 @@ end
 
 #extension  - Part 3: 
 #create an input so that you can edit the list / save wishlists etc to file/database
+#input (part3) would ask people to select which group they were part of, or to add a new group if not there?
 
 #extension  - part 4: 
 #the output will be blind, so that if you type in your name, you are given 
