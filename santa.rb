91c94317a306247a97d9633a9841e4ee73bc7348
docljn <ljnoble@gmail.com>
2017-08-24 22:56:48 +0100

    collect method now adds person class variables to an array

--- santa.rb
+++ santa.rb
@@ -25,31 +25,35 @@
 #start thinking about classes?
 #Person(name, clan, wishlist)??
 #a way to select people by clan
-#how to find out what the clans are? 
+#how to find out what the clans are?
 
 #but I want to get user input to create a person, HOW.
 
-class Person 
- 
-	attr_accessor(:pname, :clan, :wishlist) 
+class Person
+
+	attr_accessor(:pname, :clan, :wishlist)
+
 
 	def initialize(pname, clan = "main", wishlist = "anything under £10")
 		@@clans ||= []
-		@@members ||= {}
+		@@members ||= []
 		@pname = pname
 		@clan = clan
-		@wishlist = wishlist 
+		@wishlist = wishlist
 	end
-	#that sets everything at initialization i.e. Person.new
-	#I also want to shove the 'clan' into an array of clans at this stage HOW??
+
+
 
 	def inspect
 		"#{@pname}"
 	end
 
+
+	#this doesn't do what I want it to
 	def collect
-	@@members[pname] = clan
+	@@members << self
 	end
+
 
 	def clan_collect
 		unless @@clans.include?(clan)
@@ -58,21 +62,20 @@ class Person
 		p @@clans
 	end
 
+
 	def self.output_clans
 		@@clans
 	end
+
 
 	def self.output_members
 		@@members
 	end
 
 
-
-
-
 end
 
-#maybe a clan class, 
+#maybe a clan class,
 #or would I be better off with a hash? that way I don't need to worry about names?
 class Clan
 	attr_accessor(:cname, :cmember)
@@ -95,58 +98,61 @@ end
 
 
 
-#create some sample people:
-#and give them different clan attributes
+#create some sample people and give them different clan attributes
 #this will eventually be done as part of accepting user data
+#keep a list of the clans used
 
+
+participants = []
 
 a1 = Person.new("a1", "a")
 a1.collect
 a1.clan_collect
 
+
 a2 = Person.new("a2", "a")
 a2.collect
 a2.clan_collect
+
 
 a3 = Person.new("a3", "a")
 a3.collect
 a3.clan_collect
 
+
 b1 = Person.new("b1", "b")
 b1.collect
 b1.clan_collect
 
+
 b2 = Person.new("b2", "b")
 b2.collect
-b2.clan_collect
+
 
 b3 = Person.new("b3", "b")
 b3.collect
 b3.clan_collect
 
+
 c1 = Person.new("c1", "c")
 c1.collect
 c1.clan_collect
+
 
 c2 = Person.new("c2", "c")
 c2.collect
 c2.clan_collect
 
 
+
+#access the array of all the Person class instances
+participants = Person.output_members
+p participants
+
+
+#access the array of all clan names
 clans = Person.output_clans
 p clans
-
-
-
-#put them all in one array (this will eventually be done as part of creating Person.new)
-participants = [a1, a2, a3, b1, b2, b3, c1, c2]
-
-#put all the clans into an array so you know what you've got
-#(this will eventually bedone as part of creating Person.new)
-
-#commented out to test clan_collect result
-#clans = ["a", "b", "c"]
-
 
 
 
@@ -169,15 +175,15 @@ c = [c1,c2]
 
 
 #shuffle the members of each tribe to give a random order
-#AND sort tribes by size (biggest last): 
+#AND sort tribes by size (biggest last):
 
-tribes = [a.shuffle,b.shuffle,c.shuffle] 
+tribes = [a.shuffle,b.shuffle,c.shuffle]
 sorted_tribes = tribes.sort_by(&:length)
 
 largest_tribe = sorted_tribes[-1]
 
 #list of all participants, in randomly ordered tribes
-people = sorted_tribes.flatten 
+people = sorted_tribes.flatten
 
 
 
@@ -199,12 +205,12 @@ end
 
 recipients = people.rotate(offset)
 
-#create a giver<->recipient reference list 
+#create a giver<->recipient reference list
 list = Hash[people.zip recipients]
 
 
 p list
-p offset 
+p offset
 
 end
 
@@ -212,7 +218,7 @@ end
 
 
 
-#extension  - Part 4: 
+#extension  - Part 4:
 #create an input so that you can edit the list
 #input would ask people to select which group they were part of, or to add a new group if not there?
 
@@ -223,8 +229,8 @@ end
 #end
 
 
-#extension  - part 5: 
-#nake a single page website where you can add name, clan, email, wishlist and 
+#extension  - part 5:
+#nake a single page website where you can add name, clan, email, wishlist and
 #the result is emailed to all the participants
 #you'll still need an organiser, so the website won't store any user data once the emails
 #have been sent, as otherwise DPA gets involved???
